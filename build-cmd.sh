1be50a78e65880940f000d8f3b01fd3ac2c04791
Rye <rye@alchemyviewer.org>
2025-09-17 19:54:45 -0400

    Add autobuild skeleton with onetbb package

---
+++ build-cmd.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+set -eux
+
+OPENVDB_SOURCE_DIR="openvdb"
+
+top="$(pwd)"
+stage="$top"/stage
+
+# load autobuild provided shell functions and variables
+case "$AUTOBUILD_PLATFORM" in
+    windows*)
+        autobuild="$(cygpath -u "$AUTOBUILD")"
+    ;;
+    *)
+        autobuild="$AUTOBUILD"
+    ;;
+esac
+source_environment_tempfile="$stage/source_environment.sh"
+"$autobuild" source_environment > "$source_environment_tempfile"
+. "$source_environment_tempfile"
+
+# remove_cxxstd apply_patch
+source "$(dirname "$AUTOBUILD_VARIABLES_FILE")/functions"
+
+apply_patch "$top/patches/fix-prebuilt-tbb-link.patch" "$OPENVDB_SOURCE_DIR"
+
+pushd "$OPENVDB_SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+
+        # ------------------------ windows, windows64 ------------------------
+        windows*)
+            load_vsvars
+
+            mkdir -p "build_debug"
+            pushd "build_debug"
+                opts="$(replace_switch /Zi /Z7 $LL_BUILD_DEBUG)"
+                plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
+
+                cmake -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" .. \
+                        -DCONCURRENT_MALLOC=None \
+                        -DOPENVDB_BUILD_BINARIES=OFF \
+                        -DOPENVDB_USE_DELAYED_LOADING=OFF \
+                        -DUSE_BLOSC=OFF \
+                        -DUSE_ZLIB=OFF \
+                        -DPREBUILT_LIB_DIR="$(cygpath -m "$stage/packages/lib/debug")" \
+                        -DPREBUILT_INCLUDE_DIR="$(cygpath -m "$stage/packages/include")" \
+                        -DCMAKE_CONFIGURATION_TYPES="Debug" \
+                        -DCMAKE_C_FLAGS_DEBUG="$plainopts" \
+                        -DCMAKE_CXX_FLAGS_DEBUG="$opts /EHsc" \
+                        -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT="Embedded" \
+                        -DCMAKE_INSTALL_PREFIX="$(cygpath -m $stage)" \
+                        -DCMAKE_INSTALL_LIBDIR="$(cygpath -m "$stage/lib/debug")" \
+                        -DCMAKE_INSTALL_BINDIR="$(cygpath -m "$stage/lib/debug")"
+
+                cmake --build . --config Debug --parallel $AUTOBUILD_CPU_COUNT
+                cmake --install . --config Debug
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    ctest -C Debug --parallel $AUTOBUILD_CPU_COUNT
+                fi
+            popd
+
+            mkdir -p "build_release"
+            pushd "build_release"
+                opts="$(replace_switch /Zi /Z7 $LL_BUILD_RELEASE)"
+                plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
+
+                cmake -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" .. \
+                        -DCONCURRENT_MALLOC=None \
+                        -DOPENVDB_BUILD_BINARIES=OFF \
+                        -DOPENVDB_USE_DELAYED_LOADING=OFF \
+                        -DUSE_BLOSC=OFF \
+                        -DUSE_ZLIB=OFF \
+                        -DPREBUILT_LIB_DIR="$(cygpath -m "$stage/packages/lib/release")" \
+                        -DPREBUILT_INCLUDE_DIR="$(cygpath -m "$stage/packages/include")" \
+                        -DCMAKE_CONFIGURATION_TYPES="Release" \
+                        -DCMAKE_C_FLAGS="$plainopts" \
+                        -DCMAKE_CXX_FLAGS="$opts /EHsc" \
+                        -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT="Embedded" \
+                        -DCMAKE_INSTALL_PREFIX="$(cygpath -m $stage)" \
+                        -DCMAKE_INSTALL_LIBDIR="$(cygpath -m "$stage/lib/release")" \
+                        -DCMAKE_INSTALL_BINDIR="$(cygpath -m "$stage/lib/release")"
+
+                cmake --build . --config Release --parallel $AUTOBUILD_CPU_COUNT
+                cmake --install . --config Release
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    ctest -C Release --parallel $AUTOBUILD_CPU_COUNT
+                fi
+            popd
+        ;;
+
+        # ------------------------- darwin, darwin64 -------------------------
+        darwin*)
+            export MACOSX_DEPLOYMENT_TARGET="$LL_BUILD_DARWIN_DEPLOY_TARGET"
+
+            for arch in x86_64 arm64 ; do
+                ARCH_ARGS="-arch $arch"
+                cc_opts="${TARGET_OPTS:-$ARCH_ARGS $LL_BUILD_RELEASE}"
+                cc_opts="$(remove_cxxstd $cc_opts)"
+                ld_opts="$ARCH_ARGS"
+
+                mkdir -p "build_$arch"
+                pushd "build_$arch"
+                    CFLAGS="$cc_opts" \
+                    LDFLAGS="$ld_opts" \
+                    cmake .. -G "Xcode" \
+                        -DCMAKE_CONFIGURATION_TYPES="Release" \
+                        -DCMAKE_C_FLAGS="$cc_opts" \
+                        -DCMAKE_CXX_FLAGS="$cc_opts" \
+                        -DCMAKE_INSTALL_PREFIX="$stage" \
+                        -DCMAKE_INSTALL_LIBDIR="$stage/lib/release/$arch" \
+                        -DCMAKE_OSX_ARCHITECTURES="$arch" \
+                        -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+                        -DCONCURRENT_MALLOC=None \
+                        -DOPENVDB_BUILD_BINARIES=OFF \
+                        -DOPENVDB_USE_DELAYED_LOADING=OFF \
+                        -DUSE_BLOSC=OFF \
+                        -DUSE_ZLIB=OFF
+
+                    cmake --build . --config Release --parallel $AUTOBUILD_CPU_COUNT
+                    cmake --install . --config Release
+
+                    # conditionally run unit tests
+                    if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                        ctest -C Release --parallel $AUTOBUILD_CPU_COUNT
+                    fi
+                popd
+            done
+
+            lipo -create -output "$stage/lib/release/libopenvdb.a" "$stage/lib/release/x86_64/libopenvdb.a" "$stage/lib/release/arm64/libopenvdb.a"
+        ;;
+
+        # -------------------------- linux, linux64 --------------------------
+        linux*)
+            # Default target per autobuild build --address-size
+            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
+
+            # Release
+            mkdir -p "build"
+            pushd "build"
+                cmake .. -GNinja \
+                    -DCMAKE_BUILD_TYPE="Release" \
+                    -DCMAKE_C_FLAGS="$(remove_cxxstd $opts)" \
+                    -DCMAKE_CXX_FLAGS="$opts" \
+                    -DCMAKE_INSTALL_PREFIX="$stage" \
+                    -DCMAKE_INSTALL_LIBDIR="$stage/lib/release" \
+                    -DCONCURRENT_MALLOC=None \
+                    -DOPENVDB_BUILD_BINARIES=OFF \
+                    -DOPENVDB_USE_DELAYED_LOADING=OFF \
+                    -DUSE_BLOSC=OFF \
+                    -DUSE_ZLIB=OFF
+
+                cmake --build . --config Release
+                cmake --install . --config Release
+
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    ctest -C Release --parallel $AUTOBUILD_CPU_COUNT
+                fi
+            popd
+        ;;
+    esac
+
+    mkdir -p "$stage/LICENSES"
+    cp LICENSE "$stage/LICENSES/openvdb.txt"
+popd
